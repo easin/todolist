b51bcb2e82e0854c0ecd6b45f607b15d4dc0ea97
yuxin <yuxinvictor@126.com>
2018-10-30 22:32:27 +0800

    犯了一个低级错误，手机是另一台pc了，也要能访问api接口，写localhost是会报错，因为手机上的localhost没有api接口

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const API_ROOT = process.env.API_ROOT || 'http://localhost:8000'
+const API_ROOT = process.env.API_ROOT || 'http://192.168.1.108:8000'
 
-axios.defaults.timeout = 5000
+axios.defaults.timeout = 3000
 axios.defaults.baseURL = API_ROOT
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
