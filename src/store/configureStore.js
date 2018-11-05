48256e4d73789d8ae89481e1204b84d751162797
yuxin <yuxinvictor@126.com>
2018-11-05 23:28:14 +0800

    加了logger 可以log action

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,19 @@
-import { createStore, compose, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 // import { routerMiddleware } from 'react-router-redux'
 import rootReducer from '../reducers'
 import rootSaga from '../sagas'
 
 export default function configureStore(history, initialState) {
-
+  const logger = createLogger(); //调试阶段使用,终端运行：react-native log-android可以查看,web控制台也可以查看
   const sagaMiddleware = createSagaMiddleware()
   const store = createStore(
     rootReducer,
     initialState,
     compose(
       applyMiddleware(
-        sagaMiddleware
+        sagaMiddleware,logger
         //,
         // routerMiddleware(history)
       )
