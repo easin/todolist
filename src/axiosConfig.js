7a2f0fab39faa4338bc4bc650e6c1298d7962bbc
yuxin <yuxinvictor@126.com>
2018-11-16 22:27:05 +0800

    网络请求拦截器，token验证

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -6,4 +6,24 @@ axios.defaults.timeout = 3000
 axios.defaults.baseURL = API_ROOT
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
+
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    console.log(' before request is sent ========>'+config.baseURL+config.url)
+    console.log(' config ========>')
+    console.log(config)
+
+    // let user = JSON.parse(localStorage.getItem('user'));
+    // if(config.url.indexOf('login')<0)//非登入接口
+    // {
+    // 	if (user && user.token) {
+    // 		config.headers.Authorization = `Bearer ${user.token}`;
+    //     // return { 'Authorization': 'Bearer ' + user.token };
+    // 	}
+    // }
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
 export default axios
