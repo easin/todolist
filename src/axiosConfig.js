3fa89163dfe389068c0b37855c1f93f064436352
yuxin <yuxinvictor@126.com>
2018-11-22 21:14:12 +0800

    测试环境

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ROOT = process.env.API_ROOT || 'http://10.0.2.2:8000'
+const API_ROOT = process.env.API_ROOT || 'http://47.98.161.226:8000'
 
 axios.defaults.timeout = 3000
 axios.defaults.baseURL = API_ROOT
