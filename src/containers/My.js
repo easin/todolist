5ba3170e8a1c875d1fd80ef0646cc422ebb65c1d
yuxin <yuxinvictor@126.com>
2018-11-28 17:05:17 +0800

    熟悉和测试各种组件

---
+++ src/containers/My.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text,View } from 'react-native';
+import { Portal,Button,IconButton,Colors } from 'react-native-paper';
+import { connect } from 'react-redux';
+class My extends React.Component {
+  render() {
+    return (
+      <Portal.Host>
+        <Text>Content of the app</Text>
+         <Button icon="add-a-photo" mode="contained" onPress={() => console.log('Pressed')}>  </Button>
+           <IconButton icon="add-a-photo" size={24} onPress={() => {}} />
+        <IconButton
+          icon="https"
+          size={24}
+          color={Colors.green500}
+          onPress={() => {}}
+        />
+      </Portal.Host>
+    );
+  }
+}
+export default  connect()(My)
