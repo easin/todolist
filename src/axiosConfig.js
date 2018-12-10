b97e809944a07ae57c44a6f8fa846b5d9388ba24
yuxin <yuxinvictor@126.com>
2018-12-10 23:14:31 +0800

    axios加了拦截器和qs使post有数据

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,14 +1,38 @@
 import axios from 'axios'
+import qs from 'qs'
 //47.98.161.226  10.0.2.2
 const API_ROOT = process.env.API_ROOT || 'http://47.98.161.226:8000'
+
 
 axios.defaults.timeout = 3000
 axios.defaults.baseURL = API_ROOT
 // axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
+
+// axios.interceptors.request.use((req) => {
+//     console.log("dddddddddddd")
+//     console.log(req)
+
+//     if (req.method === 'post') {
+//      // req.data = qs.stringify(req.data);
+
+//     }
+//     return req;
+// }, (error) => Promise.reject(error));
+
 axios.interceptors.request.use(function (config) {
     // Do something before request is sent
+
+    
+    // Accept:"application/json, text/plain, */*" 指定客户端只能接受 json的
+
+    //&& config.headers['Content-Type'] === 'application/x-www-form-urlencoded'
+    if (config.method === 'post' && config.data) {
+          config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+          config.data = qs.stringify(config.data);
+    }
+
     console.log(' before request is sent ========>'+config.baseURL+config.url)
     console.log(' config ========>')
     console.log(config)
